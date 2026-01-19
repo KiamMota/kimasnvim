bfe92959834014f76e1fce60c8386e7fb0219d4d
Kiam Mota <133913407+KiamMota@users.noreply.github.com>
2026-01-19 10:24:55 -0300

    Update autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,23 +1,57 @@
--- flags usadas para correção do meu sistema
-
 local corrections = {
-  ["include"] = {"INcluide", "incldue", "inclide", "Include", "incblduye", "incluyde"},
-  ["length"] = {"lenght", "lengh"},
-  ["namespace"] = {"namesapce", "namespce"},
-  ["return"] = {"retun", "retrn"},
-  ["width"] = {"witdht", "widht", "witdh"},
-  ["function"] = {"fucntion", "functoin"},
-  ["const"] = {"constr", "cosnt"},
+  -- pré-processador
+  include   = {"INcluide", "incldue", "inclide", "Include", "incblduye", "incluyde"},
+  define    = {"DEFINE", "deifnde", "deinfe", "dei0fne"},
+  endif     = {"endfi", "ednif"},
+  ifdef     = {"ifdfe", "iddef"},
+  ifndef    = {"ifndfe", "infdef"},
+
+  -- estrutura
+  namespace = {"namesapce", "namespce"},
   ["class"] = {"calss", "lcass", "clas"},
-  ["void"] = {"voud", "vodi"},
-  ["define"] = {"DEFINE", "deifnde", "deinfe", "dei0fne"},
-  ["while"] = {"wilhe", "wihle", "whie"},
-  ["switch"] = {"siwtch", "swithc", "esicjh", "sich", "swtich,", "stich"},
-  ["virtual"] = {"virutal", "virutla", "virtula", "viutal"}
+  struct    = {"strcut", "sturct"},
+  virtual   = {"virutal", "virutla", "virtula", "viutal"},
+  public    = {"publci", "pubilc"},
+  private   = {"pritave", "privtae"},
+  protected = {"protcted", "prtoected"},
+
+  -- tipos
+  ["const"] = {"constr", "cosnt"},
+  void      = {"voud", "vodi"},
+  bool      = {"bol", "boool"},
+  size_t    = {"sizet", "size-t"},
+  string    = {"stirng", "strign"},
+
+  -- fluxo
+  ["return"] = {"retun", "retrn"},
+  ["while"]  = {"wilhe", "wihle", "whie"},
+  ["switch"] = {"siwtch", "swithc", "esicjh", "sich", "swtich", "stich"},
+  ["case"]   = {"caes"},
+  default    = {"defualt", "defautl"},
+  break      = {"breka", "berak"},
+  continue   = {"contiune", "conitnue"},
+
+  -- funções / STL comuns
+  function = {"fucntion", "functoin"},
+  length   = {"lenght", "lengh"},
+  width    = {"witdht", "widht", "witdh"},
+  begin    = {"begni"},
+  end      = {"edn"},
 }
+
+local function in_string()
+  local syn = vim.fn.synIDattr(
+    vim.fn.synID(vim.fn.line("."), vim.fn.col(".") - 1, 1),
+    "name"
+  )
+  return syn:match("String") ~= nil
+end
 
 for correct, wrongs in pairs(corrections) do
   for _, wrong in ipairs(wrongs) do
-    vim.cmd(string.format("iabbrev %s %s", wrong, correct))
+    vim.cmd(string.format(
+      [[iabbrev <expr> %s (in_string() and "%s" or "%s")]],
+      wrong, wrong, correct
+    ))
   end
 end
