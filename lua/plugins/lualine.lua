4bad9ff47bbf984f01361707b894182a16ba0499
KiamMota <somentekiam@gmail.com>
2026-07-03 14:11:39 -0300

    refeito

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,8 +9,8 @@ require('lualine').setup({
   options           = {
     icons_enabled        = true,
     theme                = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators   = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    section_separators   = { left = '', right = '' },
     disabled_filetypes   = { statusline = {}, winbar = {} },
     always_divide_middle = true,
     always_show_tabline  = true,
---
+++ lua/plugins/starter.lua
@@ -0,0 +1,3 @@
+vim.pack.add({
+  "https://github.com/goolord/alpha-nvim",
+})
